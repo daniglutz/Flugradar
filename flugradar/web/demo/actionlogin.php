23f4ef62e62059880f2711458d88c330ace7ff8a
Dario Kuster <>
2017-01-03 08:52:19 +0100

    - Formatierung verbessert - nicht mehr verwendete Daten gelöscht

--- flugradar/web/demo/actionlogin.php
+++ flugradar/web/demo/actionlogin.php
@@ -1,111 +1,131 @@
 <?php
-/**
- * File actionlogin
- * The actionlogin can vallidate the login data and give access to the application.
- * 
- * @name actionlogin.php
- * @author Andreas Trachsel
- * @version 28.12.2016
- *  
- */
-
-
-/** ** database class ** */
-include_once './classes/Database.class.php';
-
-
-// take session
-session_start();
-
-// check user data
-userValidate();    
     
-// login ok
-header("Location: ./?site=departures&airport=LSZH");
+    /**
+     * can vallidate the login data and give access to the application
+     * 
+     * @name    actionlogin.php
+     * @author  Andreas Trachsel
+     * @version 28.12.2016
+     *  
+     */
 
 
-//-------functions-------
-
-function inSession() {
-    //is not in the session
-    if (!isset($_SESSION['user'])) {
-        exit("<p>Kein Zugang<br /><a href='login.php'>Zum Login</a></p>");
-    }    
-}
+    /** ** database class ** */
+    include_once './classes/Database.class.php';
 
 
-function userValidate() {
-    // variables for access validate
-    $bUser = false;
-    $bPwd = false;
+    // take session
+    session_start();
+
+    // check user data
+    userValidate($_POST['user'], $_POST['pwd']);    
+
+    // login ok
+    header("Location: ./?site=departures&airport=LSZH");
     
-    // create database object
-    $db = new Database();
     
-    //define SQL query
-    $sql = "
-    SELECT 
-        `id`,
-        `username`,
-        `password`,
-        `admin`
-    FROM `users`            
-    WHERE `username` = '".$_POST['user']."'";
-    
-    // *** run query ***
-    $result = $db->query($sql);
+	/**
+	* check whether user and password are valid
+	* 
+	* @author  Andreas Trachsel
+	* @version 28.12.2016
+	* 
+	* @param   string user
+	* @param   string password
+	* @return  void
+	*/
+    function userValidate($user, $pw) {
+        // variables for access validate
+        $userValid = false;
+        $pwValid = false;
 
-    // *** results? ***
-    if($result->num_rows) {
-        // ** save result **
-        $row = $result->fetch_assoc();
-        
-        // check username
-        if($row['username'] == $_POST['user']) {
-            $bUser = true;
-        }
-        // check password
-        if(password_verify($_POST['pwd'], $row['password'])) {
-            $bPwd = true;
-        }
-        
-        if($bUser AND $bPwd) {
-            setSession($row);
+        // create database object
+        $db = new Database();
+
+        //define SQL query
+        $sql = "
+        SELECT 
+            `id`,
+            `username`,
+            `password`,
+            `admin`
+        FROM `users`            
+        WHERE `username` = '".$user."'";
+
+        // *** run query ***
+        $result = $db->query($sql);
+
+        // *** results? ***
+        if($result->num_rows) {
+            // ** save result **
+            $row = $result->fetch_assoc();
+
+            // check username
+            if($row['username'] == $user) {
+                $userValid = true;
+            }
+            // check password
+            if(password_verify($pw, $row['password'])) {
+                $pwValid = true;
+            }
+
+            if($userValid AND $pwValid) {
+                setSession($row);
+            }
+            else {
+                setError($userValid, $pwValid);
+            }
         }
         else {
-            error($bUser, $bPwd);
+            setError($userValid, $pwValid);
         }
     }
-    else {
-        error($bUser, $bPwd);
-    }
-}
-
-function setSession($row) {
-    $_SESSION['id'] = $row['id'];
-    $_SESSION['user'] = $row['username'];
-    $_SESSION['admin'] = $row['admin'];
-}
-
-// validate access
-function error($bUser, $bPwd) {
     
-    if($bUser == false) {
-        $_SESSION['error'] = "
-        <div class='alert alert-danger' role='alert'>
-            <span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>
-            <span class='sr-only'>Hinweis:</span>
-            <b>Benutzer nicht vorhanden</b><br />
-            Bitte geben Sie einen gültigen Benutzernamen ein.
-        </div>";
+	/**
+	* set session
+	* 
+	* @author  Andreas Trachsel
+	* @version 28.12.2016
+	* 
+	* @param   array query result
+	* @return  void
+	*/
+    function setSession($row) {
+        $_SESSION['id'] = $row['id'];
+        $_SESSION['user'] = $row['username'];
+        $_SESSION['admin'] = $row['admin'];
     }
-    elseif($bPwd == false) {
-        $_SESSION['error'] = "
-        <div class='alert alert-danger' role='alert'>
+    
+	/**
+	* set error
+	* 
+	* @author  Andreas Trachsel
+	* @version 28.12.2016
+	* 
+	* @param   boolean user valid (true or false)
+	* @param   boolean password valid (true or false)
+	* @return  void
+	*/
+    function setError($userValid, $pwValid) {
+        // set error
+        if($userValid == false) {
+            $_SESSION['error'] = "
+            <div class='alert alert-danger' role='alert'>
                 <span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>
                 <span class='sr-only'>Hinweis:</span>
-                <b>Passwort ungültig</b><br />
-                Bitte geben Sie ein gültiges Passwort ein.
-        </div>";
+                <b>Benutzer nicht vorhanden</b><br />
+                Bitte geben Sie einen gültigen Benutzernamen ein.
+            </div>";
+        }
+        elseif($pwValid == false) {
+            $_SESSION['error'] = "
+            <div class='alert alert-danger' role='alert'>
+                    <span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>
+                    <span class='sr-only'>Hinweis:</span>
+                    <b>Passwort ungültig</b><br />
+                    Bitte geben Sie ein gültiges Passwort ein.
+            </div>";
+        }
     }
-}
+    
+?>
